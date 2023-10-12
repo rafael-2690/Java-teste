e71de57908edc320113ae3716128e35257d41fdc
Rafael <pereira.fael.pereira@gmail.com>
2023-10-12 18:15:36 -0300

    .

---
+++ javaTeste/src/Banco.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Banco {
+
+  public static void main(String[] args) {
+    // Lendo os dados de Entrada:
+    Scanner scanner = new Scanner(System.in);
+    int numeroConta = scanner.nextInt();
+    scanner.nextLine(); // Consome a quebra de linha após a entrada do número da conta
+    String nomeTitular = scanner.nextLine();
+    double saldo = scanner.nextDouble();
+
+    //TODO: Criar uma instância de "ContaBancaria" com os valores de Entrada.
+    ContaBancaria ContaBancaria = new ContaBancaria();
+
+
+    
+    
+    System.out.println("Informacoes:");
+    //TODO: Imprimir as informações da conta usando o objeto criado no TODO acima.
+    System.out.println("Conta: " + numeroConta);
+    System.out.println("Titular: " + nomeTitular);
+    System.out.println("Informacoes: " + saldo);
+  }
+}
+
+class ContaBancaria {
+  int numero;
+  String titular;
+  double saldo;
+
+  public void Bancaria(int numero, String titular, double saldo) {
+    this.numero = numero;
+    this.titular = titular;
+    this.saldo = saldo;
+  }
+  }
+  
+
